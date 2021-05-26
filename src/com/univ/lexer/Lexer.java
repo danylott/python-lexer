19aca8b77cd40dcdc5646d73b0115a032f87ffdb
d.tuytuyshkin@gmail.com <d.tuytuyshkin@gmail.com>
2021-05-26 10:24:55 +0300

    Added interface for Lexer & simple test.py file

---
+++ src/com/univ/lexer/Lexer.java
@@ -0,0 +1,31 @@
+package com.univ.lexer;
+
+import com.univ.lexer.token.Token;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lexer {
+    private List<Token> tokens;
+    private List<String> sourceCode;
+
+    public Lexer() {
+        tokens = new ArrayList<>();
+    }
+
+    public List<Token> tokenize(String filepath) throws IOException {
+        tokens.clear();
+        sourceCode = readFileData(filepath);
+
+        return tokens;
+    }
+
+    private List<String> readFileData(String filepath) throws IOException {
+        List<String> sourceCodeLines = Files.readAllLines(Paths.get(filepath), StandardCharsets.UTF_8);
+        return sourceCodeLines;
+    }
+}
---
+++ src/com/univ/test.py
@@ -0,0 +1,5 @@
+import numpy
+a = 3+2
+# comment
+b = 3+3
+b = 1-2
